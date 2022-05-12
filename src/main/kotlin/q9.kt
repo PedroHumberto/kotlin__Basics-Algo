2da6085b1f8557dfd7ef098b03304e4dd4ff941b
Pedro Humberto <pedrohumbertogc@gmail.com>
2022-05-11 21:53:14 -0300

    q9 comentarios e readline

--- src/main/kotlin/q9.kt
+++ src/main/kotlin/q9.kt
@@ -5,6 +5,8 @@ class Data(val dia: Int, val mes: Int, val ano: Int) {
             bissexto = true
         }
     }
+
+    //validação de datas
     init {
         if (dia > 31 || mes > 12) {
             throw AssertionError("Data Invalida")
@@ -25,7 +27,6 @@ class Data(val dia: Int, val mes: Int, val ano: Int) {
             println("Não é Bissexto")
         }
     }
-
     fun qtDias(){
         when(mes){
             4,6,9,11 -> println("tem 30 Dias")
@@ -39,6 +40,7 @@ class Data(val dia: Int, val mes: Int, val ano: Int) {
             else -> println("Tem 31 Dias")
         }
     }
+    //distancia entre datas
     fun contagemDias(){
         var dias: Int = 0
         var somaDias: Int = 0
@@ -58,17 +60,22 @@ class Data(val dia: Int, val mes: Int, val ano: Int) {
         }
 
         var distanciaDias = somaDias - (dias - dia) - 1
-        println("A distancia entre o dia 01/01/$ano até a data selecionada é de: $distanciaDias dias")
+        println("A distancia entre o dia 01/01/$ano até a data $dia/$mes/$ano é de: $distanciaDias dias")
 
     }
 
 }
 fun main(){
-    val data1 = Data(31, 12, 2024)
+    println("Digite um dia")
+    var dia = readln().toInt()
+    println("Digite um mês Ex:1,2,3..")
+    var mes = readln().toInt()
+    println("Digite um ano Ex:2024")
+    var ano = readln().toInt()
+    val data1 = Data(dia, mes, ano)
 
     data1.anoBissexto()
     data1.qtDias()
     data1.contagemDias()
-
 
 }
